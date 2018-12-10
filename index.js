32f7d74279f48faa9af97cf5f31ff1c6e1f15675
Simo Ahava <simo.s.ahava@gmail.com>
2018-12-10 13:07:09 +0200

    Multisite + BQ write

---
+++ index.js
@@ -0,0 +1,80 @@
+const puppeteer = require('puppeteer');
+const lighthouse = require('lighthouse');
+const {URL} = require('url');
+const config = require('./config.json');
+const {BigQuery} = require('@google-cloud/bigquery');
+const fs = require('fs');
+const uuidv1 = require('uuid/v1');
+const {promisify} = require('util');
+const writeFile = promisify(fs.writeFile);
+
+const bigquery = new BigQuery({
+  projectId: 'multisite-lighthouse-gcp'
+});
+
+async function launchLighthouse(url) {
+  console.log(`Starting browser for ${url}`);
+
+  const browser = await puppeteer.launch({args: ['--no-sandbox']});
+
+
+  console.log(`Browser started for ${url}`);
+
+  browser.on('targetchanged', async target => {
+    const page = await target.page();
+
+    if (page && page.url() === url) {
+      // Do something with network conditions
+      /*
+      const client = await page.target().createCDPSession();
+      await client.send('Runtime.evaluate', {
+        expression: `(${addStyleContent.toString()})('${css}')`
+      });*/
+    }
+  });
+
+  config.lighthouseFlags.port = (new URL(browser.wsEndpoint())).port;
+  console.log(`Starting lighthouse for ${url}`);
+
+  const lhr = await lighthouse(url, config.lighthouseFlags);
+
+  console.log(`Lighthouse done for ${url}`);
+
+  await browser.close();
+
+  console.log(`Browser closed for ${url}`);
+
+  return lhr;
+}
+
+/*DEBUG
+(async() => {
+  await Promise.all(config.url.map(async (url) => {
+    const uuid = uuidv1();
+    const res = await launchLighthouse(url);
+    await writeFile(`./${uuid}.csv`, res.report);
+  }));
+})();
+*/
+
+exports.launchLighthouse = async (data, callback) => {
+  console.log('Received message, starting...');
+
+  await Promise.all(config.url.map(async (url) => {
+    const uuid = uuidv1();
+    const res = await launchLighthouse(url);
+    const metadata = {
+      sourceFormat: 'CSV',
+      skipLeadingRows: 1,
+      autodetect: true
+    };
+    await writeFile(`/tmp/${uuid}.csv`, res.report);
+    console.log(`Loading result from ${url} to BigQuery`);
+    const bqload = await bigquery
+      .dataset('lighthouse')
+      .table('test')
+      .load(`/tmp/${uuid}.csv`, metadata);
+    console.log(bqload[0].status.state);
+  }));
+
+};
